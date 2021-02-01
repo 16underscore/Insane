51aed193be9bed09d71b1b56f53d78ebe22a82e1
16_ <omicron.mc.de@gmail.com>
2021-02-01 20:13:37 +0100

    Stuff

--- src/main/java/me/sixteen_/insane/module/modules/combat/Killaura.java
+++ src/main/java/me/sixteen_/insane/module/modules/combat/Killaura.java
@@ -1,19 +1,24 @@
 package me.sixteen_.insane.module.modules.combat;
 
 import java.util.Iterator;
+import java.util.Map.Entry;
+
+import com.google.common.collect.Multimap;
 
 import me.sixteen_.insane.module.Module;
 import me.sixteen_.insane.module.ModuleCategory;
 import me.sixteen_.insane.utils.Logger;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.item.TooltipContext;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.util.ClientPlayerTickable;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.text.Text;
 
 /**
  * @author 16_
@@ -21,6 +26,7 @@ import net.minecraft.text.Text;
 public class Killaura extends Module implements ClientPlayerTickable {
 
 	private MinecraftClient mc;
+	private ClientPlayerEntity player;
 	private boolean pause;
 	private float range;
 
@@ -31,6 +37,7 @@ public class Killaura extends Module implements ClientPlayerTickable {
 	@Override
 	protected void onEnable() {
 		mc = MinecraftClient.getInstance();
+		player = mc.player;
 		range = 6F;
 		pause = false;
 	}
@@ -44,7 +51,6 @@ public class Killaura extends Module implements ClientPlayerTickable {
 
 	@Override
 	public void onUpdate() {
-		final ClientPlayerEntity player = mc.player;
 		final Iterator<Entity> it = mc.world.getEntities().iterator();
 		Entity entity;
 		while (it.hasNext()) {
@@ -55,7 +61,7 @@ public class Killaura extends Module implements ClientPlayerTickable {
 					if (player.distanceTo(le) < range) {
 						if (!le.isDead()) {
 							if (le.hurtTime == 0) {
-								if (le.getHealth() < attackDamage(player, le)) {
+								if (le.getHealth() <= attackDamage(le)) {
 									mc.interactionManager.attackEntity(player, le);
 									pause = false;
 								} else if (player.getAttackCooldownProgress(0) == 1) {
@@ -75,33 +81,41 @@ public class Killaura extends Module implements ClientPlayerTickable {
 		}
 	}
 
-	private float attackDamage(final ClientPlayerEntity player, final LivingEntity target) {
-		// Attack damage
-		Iterator<Text> it = player.getMainHandStack().getTooltip(player, TooltipContext.Default.NORMAL).iterator();
-		float f = 1.0F;
-		while (it.hasNext()) {
-			final String text = it.next().getString();
-			if (text.contains("Attack Damage")) {
-				f = Float.parseFloat(text.split(" ")[1]);
-				Logger.getLogger().addChatMessage(f + "");
-			}
-		}
-//		float f = (float) player.getAttributeValue(EntityAttributes.GENERIC_ATTACK_DAMAGE);
-		// Enchanting attack damage
-		float h = EnchantmentHelper.getAttackDamage(player.getMainHandStack(), target.getGroup());
-		// Cooldown
-		final float i = player.getAttackCooldownProgress(0.5F);
-		f -= h;
-		f *= 0.2F + i * i * 0.8F;
-		h *= i;
-		// Critical
-		final boolean bl3 = (i > 0.9F && player.fallDistance > 0.0F && !player.isOnGround() && !player.isClimbing() && !player.isTouchingWater()
+	private float attackDamage(final LivingEntity target) {
+		float damage;
+		float ench;
+		float cool;
+		damage = attackDamage();
+		Logger.getLogger().addChatMessage(damage + "", true);
+		ench = EnchantmentHelper.getAttackDamage(player.getMainHandStack(), target.getGroup());
+		cool = player.getAttackCooldownProgress(0.5F);
+		damage *= 0.2F + cool * cool * 0.8F;
+		ench *= cool;
+		final boolean crit = (cool > 0.9F && player.fallDistance > 0.0F && !player.isOnGround() && !player.isClimbing() && !player.isTouchingWater()
 				&& !player.hasStatusEffect(StatusEffects.BLINDNESS) && !player.hasVehicle() && target instanceof LivingEntity) && !player.isSprinting();
-		if (bl3) {
-			f *= 1.5F;
+		if (crit) {
+			damage *= 1.5F;
 		}
-		// Sum together
-		f += h;
-		return f;
+		damage += ench;
+		return damage;
 	}
+
+	private float attackDamage() {
+		final float damageWithHand = (float) player.getAttributeBaseValue(EntityAttributes.GENERIC_ATTACK_DAMAGE);
+		Multimap<EntityAttribute, EntityAttributeModifier> map = player.getMainHandStack().getAttributeModifiers(EquipmentSlot.MAINHAND);
+		if (!map.isEmpty()) {
+			Entry<EntityAttribute, EntityAttributeModifier> entry = map.entries().iterator().next();
+			EntityAttributeModifier e = (EntityAttributeModifier) entry.getValue();
+			final float damageWithItem = (float) e.getValue();
+			return damageWithHand + damageWithItem;
+		}
+		return damageWithHand;
+	}
+	/*
+	 * Buggy version
+	 * 
+	 * private float genericAttackDamage() { float f = 1F; Iterator<Text> it = player.getMainHandStack().getTooltip(player,
+	 * TooltipContext.Default.NORMAL).iterator(); while (it.hasNext()) { final String text = it.next().getString(); if
+	 * (text.contains("Attack Damage")) { f = Float.parseFloat(text.split(" ")[1]); } } return f; }
+	 */
 }
