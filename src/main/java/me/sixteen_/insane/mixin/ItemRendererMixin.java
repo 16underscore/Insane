807e4fa9d6d03aab377daa05cb77884ab46ad180
16_ <omicron.mc.de@gmail.com>
2021-02-07 17:22:55 +0100

    Fix the real bug

--- src/main/java/me/sixteen_/insane/mixin/ItemRendererMixin.java
+++ src/main/java/me/sixteen_/insane/mixin/ItemRendererMixin.java
@@ -21,7 +21,7 @@ import net.minecraft.item.ItemStack;
 @Mixin(ItemRenderer.class)
 public abstract class ItemRendererMixin {
 
-	@Inject(method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;Lorg/spongepowered/asm/mixin/injection/callback/CallbackInfo;)V", at = @At(target = "Lnet/minecraft/client/render/item/ItemRenderer;renderBakedItemModel(Lnet/minecraft/client/render/model/BakedModel;Lnet/minecraft/item/ItemStack;IILnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;)V", value = "INVOKE", shift = Shift.BEFORE))
+	@Inject(method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V", at = @At(target = "Lnet/minecraft/client/render/item/ItemRenderer;renderBakedItemModel(Lnet/minecraft/client/render/model/BakedModel;Lnet/minecraft/item/ItemStack;IILnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;)V", value = "INVOKE", shift = Shift.BEFORE))
 	private void renderItem(ItemStack stack, ModelTransformation.Mode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, BakedModel model, final CallbackInfo info) {
 		final Inspect inspect = (Inspect) Insane.getInsane().getModuleManager().getModule(Inspect.class);
 		if (inspect.isEnabled() && renderMode.isFirstPerson()) {
