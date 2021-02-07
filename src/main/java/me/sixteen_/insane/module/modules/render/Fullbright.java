14faa1ddd927137c75634c3b73d0c602c4383c6f
16_ <omicron.mc.de@gmail.com>
2021-02-07 16:56:50 +0100

    Remove fullbright night vision mode

--- src/main/java/me/sixteen_/insane/module/modules/render/Fullbright.java
+++ src/main/java/me/sixteen_/insane/module/modules/render/Fullbright.java
@@ -9,34 +9,18 @@ import net.minecraft.client.MinecraftClient;
  */
 public class Fullbright extends Module {
 
-	private Mode mode;
 	private MinecraftClient mc;
 	private double previousGamma;
 
 	public Fullbright() {
 		super("Fullbright", ModuleCategory.RENDER);
-		mode = Mode.GAMMA;
 	}
 
 	@Override
 	protected void onEnable() {
-		switch (mode) {
-		case GAMMA:
-			gamma();
-			break;
-		case NIGHT_VISION:
-			night();
-			break;
-		}
-	}
-
-	private void gamma() {
 		mc = MinecraftClient.getInstance();
 		previousGamma = mc.options.gamma;
 		mc.options.gamma = 69.0D;
-	}
-
-	private void night() {
 	}
 
 	@Override
@@ -47,33 +31,5 @@ public class Fullbright extends Module {
 	@Override
 	protected void onShutdown() {
 		mc.options.gamma = previousGamma;
-	}
-
-	@Override
-	public void setMode(String s) {
-		for (Mode m : Mode.values()) {
-			if (m.toString().equalsIgnoreCase(s)) {
-				onDisable();
-				mode = m;
-				onEnable();
-				return;
-			}
-		}
-	}
-
-	private enum Mode {
-
-		GAMMA("Gamma"), NIGHT_VISION("Nightvision");
-
-		private final String modeName;
-
-		private Mode(String modeName) {
-			this.modeName = modeName;
-		}
-
-		@Override
-		public String toString() {
-			return modeName;
-		}
 	}
 }
