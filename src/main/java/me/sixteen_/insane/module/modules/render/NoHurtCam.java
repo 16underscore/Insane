a82fbe03e854ec5b8b73ef1de1abae66db8cd768
16_ <omicron.mc.de@gmail.com>
2021-02-15 15:56:42 +0100

    Add author

--- src/main/java/me/sixteen_/insane/module/modules/render/NoHurtCam.java
+++ src/main/java/me/sixteen_/insane/module/modules/render/NoHurtCam.java
@@ -2,7 +2,13 @@ package me.sixteen_.insane.module.modules.render;
 
 import me.sixteen_.insane.module.Module;
 import me.sixteen_.insane.module.ModuleCategory;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
+/**
+ * @author 16_
+ */
+@Environment(EnvType.CLIENT)
 public class NoHurtCam extends Module {
 
 	public NoHurtCam() {
