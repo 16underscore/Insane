5ab47af0ed3db92ffea6f485dd991c7cf1613ac7
16_ <omicron.mc.de@gmail.com>
2021-02-18 20:23:16 +0100

    Small changes

--- src/main/java/me/sixteen_/insane/module/modules/combat/Criticals.java
+++ src/main/java/me/sixteen_/insane/module/modules/combat/Criticals.java
@@ -4,7 +4,9 @@ import me.sixteen_.insane.module.Module;
 import me.sixteen_.insane.module.ModuleCategory;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.util.ActionResult;
 
 /**
  * @author 16_
@@ -14,10 +16,16 @@ public final class Criticals extends Module {
 
 	public Criticals() {
 		super("Criticals", ModuleCategory.COMBAT);
+		AttackEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+			if (isEnabled()) {
+				onUpdate();
+			}
+			return ActionResult.PASS;
+		});
 	}
 
 	@Override
-	public final void onUpdate() {
+	public void onUpdate() {
 		final double posX = mc.player.getX(), posY = mc.player.getY(), posZ = mc.player.getZ();
 		sendPos(posX, posY + 0.05D, posZ, true);
 		sendPos(posX, posY, posZ, false);
