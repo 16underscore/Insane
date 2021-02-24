4aa9f437c699ee0de1c4c4a99d4121ee4dcbe259
16_ <omicron.mc.de@gmail.com>
2021-02-24 21:37:37 +0100

    Improve config

--- src/main/java/me/sixteen_/insane/config/Config.java
+++ src/main/java/me/sixteen_/insane/config/Config.java
@@ -6,9 +6,16 @@ import java.io.FileWriter;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import me.sixteen_.insane.Insane;
+import me.sixteen_.insane.value.ranges.IntegerRange;
+import me.sixteen_.insane.value.values.BooleanValue;
+import me.sixteen_.insane.value.values.DoubleValue;
+import me.sixteen_.insane.value.values.FloatValue;
+import me.sixteen_.insane.value.values.IntegerValue;
+import me.sixteen_.insane.value.values.ListValue;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.loader.api.FabricLoader;
@@ -39,7 +46,22 @@ public final class Config {
 			if (m.hasValues()) {
 				final JsonObject value = new JsonObject();
 				m.getValues().forEach(v -> {
-					value.addProperty(v.getName(), v.toString());
+					if (v instanceof IntegerValue) {
+						value.addProperty(v.getName(), ((IntegerValue) v).getValue());
+					} else if (v instanceof FloatValue) {
+						value.addProperty(v.getName(), ((FloatValue) v).getValue());
+					} else if (v instanceof DoubleValue) {
+						value.addProperty(v.getName(), ((DoubleValue) v).getValue());
+					} else if (v instanceof BooleanValue) {
+						value.addProperty(v.getName(), ((BooleanValue) v).getValue());
+					} else if (v instanceof ListValue) {
+						value.addProperty(v.getName(), ((ListValue) v).getValue());
+					} else if (v instanceof IntegerRange) {
+						final JsonArray range = new JsonArray();
+						range.add(((IntegerRange) v).getMinValue());
+						range.add(((IntegerRange) v).getMaxValue());
+						value.add(v.getName(), range);
+					}
 				});
 				module.add("value", value);
 			}
